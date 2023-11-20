043ee50b1ffc3671c624c9fac419752e51d5ae90
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-11-20 15:51:51 -0300

    Add entity follwer

---
+++ src/main/java/org/matheuscordeiro/socialapi/domain/model/Follower.java
@@ -0,0 +1,22 @@
+package org.matheuscordeiro.socialapi.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "followers")
+@Data
+public class Follower {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+}
