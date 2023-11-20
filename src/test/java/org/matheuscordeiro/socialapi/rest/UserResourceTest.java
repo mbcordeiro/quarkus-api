044de153b254e7f2f1421310ac115654fcddd6c0
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-11-20 19:21:50 -0300

    Test errors create user

--- src/test/java/org/matheuscordeiro/socialapi/rest/UserResourceTest.java
+++ src/test/java/org/matheuscordeiro/socialapi/rest/UserResourceTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.matheuscordeiro.socialapi.rest.dto.CreateUserRequest;
+import org.matheuscordeiro.socialapi.rest.dto.ResponseError;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,5 +37,24 @@ class UserResourceTest {
                         .extract().response();
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("should return error when json is not valid")
+    @Order(2)
+    void shouldCreateUserValidationError(){
+        var user = new CreateUserRequest(null, null);
+        var response = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post(apiURL)
+                .then()
+                .extract().response();
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
     }
 }
